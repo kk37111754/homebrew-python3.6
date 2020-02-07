a37e9d4e8f991170248e918b8b9a9a0bfa1286a6
Neal Richardson <neal.p.richardson@gmail.com>
2020-02-07 10:37:43 -0800

    apache-arrow 0.16.0

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,19 +1,23 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://archive.apache.org/dist/arrow/arrow-0.15.1/apache-arrow-0.15.1.tar.gz"
-  mirror "https://www-eu.apache.org/dist/arrow/arrow-0.15.1/apache-arrow-0.15.1.tar.gz"
-  sha256 "9a2c58c72310eafebb4997244cbeeb8c26696320d0ae3eb3e8512f75ef856fc9"
+  url "https://archive.apache.org/dist/arrow/arrow-0.16.0/apache-arrow-0.16.0.tar.gz"
+  mirror "https://www-eu.apache.org/dist/arrow/arrow-0.16.0/apache-arrow-0.16.0.tar.gz"
+  sha256 "261992de4029a1593195ff4000501503bd403146471b3168bd2cc414ad0fb7f5"
+
+  patch do
+    url "https://github.com/apache/arrow/commit/7e24a84d.patch"
+    sha256 "50cf74f10543a5eea667af34de010e1040d6209d8efcf4d4586510961ac08912"
+  end
 
   bottle do
     cellar :any
-    sha256 "d5c53fd27fb63e7404b476c3f52eda5509e80313a1b189df624665f3dbddab7e" => :el_capitan_or_later
+    sha256 "6127d77c15ca02786364fe0fef5b3fb63dcfc83afdd6a0af18cb20097f6baaca" => :el_capitan_or_later
     root_url "https://autobrew.github.io/bottles"
   end
 
   depends_on "cmake" => :build
   depends_on "flatbuffers" => :build
-  depends_on "double-conversion"
   depends_on "boost"
   depends_on "lz4"
   depends_on "thrift"
@@ -22,17 +26,20 @@ class ApacheArrow < Formula
   def install
     ENV.cxx11
     args = %W[
-      -DARROW_PARQUET=ON
-      -DARROW_PLASMA=OFF
+      -DARROW_COMPUTE=ON
+      -DARROW_CSV=ON
+      -DARROW_DATASET=ON
+      -DARROW_FILESYSTEM=ON
       -DARROW_HDFS=OFF
-      -DARROW_BUILD_TESTS=OFF
-      -DARROW_TEST_LINKAGE="static"
+      -DARROW_JSON=ON
+      -DARROW_PARQUET=ON
       -DARROW_BUILD_SHARED=OFF
       -DARROW_JEMALLOC=OFF
-      -DARROW_WITH_BROTLI=OFF
-      -DARROW_USE_GLOG=OFF 
+      -DARROW_USE_GLOG=OFF
       -DARROW_PYTHON=OFF
-      -DARROW_WITH_ZSTD=OFF
+      -DARROW_S3=OFF
+      -DARROW_WITH_LZ4=ON
+      -DARROW_WITH_ZLIB=ON
       -DARROW_WITH_SNAPPY=ON
       -DARROW_BUILD_UTILITIES=ON
       -DPARQUET_BUILD_EXECUTABLES=ON
