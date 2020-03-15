fd7f2bc779e39f3b090776e6009a8e8aa498c827
Jeroen Ooms <jeroenooms@gmail.com>
2020-03-15 04:21:50 +0100

    mariadb-connector-c: disable plugins

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -1,13 +1,13 @@
 class MariadbConnectorC < Formula
   desc "MariaDB database connector for C applications"
   homepage "https://downloads.mariadb.org/connector-c/"
-  url "https://downloads.mariadb.org/f/connector-c-3.1.6/mariadb-connector-c-3.1.6-src.tar.gz"
-  sha256 "2ca368fd79e87e80497a5c9fd18922d8316af8584d87cecb35bd5897cb1efd05"
+  url "https://downloads.mariadb.org/f/connector-c-3.1.7/mariadb-connector-c-3.1.7-src.tar.gz"
+  sha256 "64f7bc8f5df3200ba6e3080f68ee4942382a33e8371baea8ca4b9242746df59a"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "18a068055ae41db853264f0272b439b7906f2998c911fb8eec719d9d04a63b57" => :el_capitan_or_later
+    sha256 "046149bed241cb5349c77dbaa891a570cd58ed7bc6dde443cc4034278112a5e3" => :el_capitan_or_later
   end
 
   depends_on "cmake" => :build
@@ -18,9 +18,10 @@ class MariadbConnectorC < Formula
 
   def install
     args = std_cmake_args
-    args << "-DWITH_OPENSSL=On"
     args << "-DOPENSSL_INCLUDE_DIR=#{Formula["openssl@1.1"].opt_include}"
-    args << "-DCOMPILATION_COMMENT=autobrew"
+
+    inreplace "plugins/auth/CMakeLists.txt", "DEFAULT DYNAMIC", "DEFAULT OFF"
+    inreplace "plugins/io/CMakeLists.txt", "DEFAULT DYNAMIC", "DEFAULT OFF"
 
     system "cmake", ".", *args
     system "make", "install"
