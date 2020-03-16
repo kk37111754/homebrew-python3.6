d566d4ab8a9d8b836d48c944fbeb2f74c232e27e
Jeroen Ooms <jeroenooms@gmail.com>
2020-03-16 16:54:49 +0100

    mariadb: enable some static plugins (#14)

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -3,11 +3,18 @@ class MariadbConnectorC < Formula
   homepage "https://downloads.mariadb.org/connector-c/"
   url "https://downloads.mariadb.org/f/connector-c-3.1.7/mariadb-connector-c-3.1.7-src.tar.gz"
   sha256 "64f7bc8f5df3200ba6e3080f68ee4942382a33e8371baea8ca4b9242746df59a"
+  revision 1
+  head "https://github.com/mariadb-corporation/mariadb-connector-c.git", :branch => 3.1
+
+  patch do
+    url "https://github.com/mariadb-corporation/mariadb-connector-c/commit/fbf1db62.patch"
+    sha256 "658b88969c147efa0fcd1c6d067fe6e7a63936d336d22d396c55fc0e268ee696"
+  end
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "046149bed241cb5349c77dbaa891a570cd58ed7bc6dde443cc4034278112a5e3" => :el_capitan_or_later
+    sha256 "9fdd9109ae52e7d8fd77957c56b1c497037837c03dba06506cc96f38d0ddf38a" => :el_capitan_or_later
   end
 
   depends_on "cmake" => :build
@@ -18,13 +25,22 @@ class MariadbConnectorC < Formula
 
   def install
     args = std_cmake_args
-    args << "-DOPENSSL_INCLUDE_DIR=#{Formula["openssl@1.1"].opt_include}"
+    args << "-DCLIENT_PLUGIN_AUTH_GSSAPI_CLIENT=STATIC"
+    args << "-DCLIENT_PLUGIN_DIALOG=STATIC"
+    args << "-DCLIENT_PLUGIN_CLIENT_ED25519=STATIC"
+    args << "-DCLIENT_PLUGIN_CACHING_SHA2_PASSWORD=STATIC"
+    args << "-DCLIENT_PLUGIN_SHA256_PASSWORD=STATIC"
+    args << "-DCLIENT_PLUGIN_MYSQL_CLEAR_PASSWORD=STATIC"
+    args << "-DCLIENT_PLUGIN_MYSQL_OLD_PASSWORD=STATIC"
+    args << "-DCLIENT_PLUGIN_REMOTE_IO=OFF"
 
-    inreplace "plugins/auth/CMakeLists.txt", "DEFAULT DYNAMIC", "DEFAULT OFF"
-    inreplace "plugins/io/CMakeLists.txt", "DEFAULT DYNAMIC", "DEFAULT OFF"
+    ENV["CFLAGS"] = "-DMYSQL_CLIENT=1"
+    ENV["OPENSSL_ROOT_DIR"] = "/usr/local/opt/openssl@1.1"
 
-    system "cmake", ".", *args
-    system "make", "install"
+    mkdir "build" do
+      system "/usr/local/bin/cmake", "..", *args
+      system "make", "install", "-j1"
+    end
   end
 
   test do
--- upload_bottles.sh
+++ upload_bottles.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # This is a hacky way to dump bottles from PR because Travis doesn't support artifacts
-org="autobrew"
-repo="bottles"
-base_url="https://api.bintray.com/content/${org}/${repo}/el-capitain/pr-${TRAVIS_PULL_REQUEST}"
-user="autobrewbot:$(rev <<< 415365001622ff1dc7292748041cac5b4d834b52)"
-for bottle in *.bottle*.*; do
-# Note replacing -- with -: https://github.com/Homebrew/brew/pull/4612#commitcomment-29995348
-target_url="$base_url/${bottle/--/-}?publish=1&override=1"
-echo "Publishing to http://dl.bintray.com/${org}/${repo}/${bottle/--/-}"
-curl --user "$user" --upload-file "$bottle" "$target_url"
+for bottle in *.bottle*.tar.gz; do
+filename="${bottle/--/-}"
+mv "${bottle}" "${filename}"
+echo "Uploading ${filename}..."
+curl -fsSF "file=@${filename}" https://file.io
+echo "Done!"
 done
