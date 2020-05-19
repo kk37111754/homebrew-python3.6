e690ff6251a8f2c235f2cab01f2ddbd7cb0bd936
Neal Richardson <neal.p.richardson@gmail.com>
2020-05-19 08:13:14 -0700

    Bump for arrow 0.17.1 (#9)
    * Bump for arrow 0.17.1, using rc
    
    * Switch arrow back to official apache URL now that 0.17.1 release happened
    
    * Add a mirror
    
    * Use last resort URL instead
    
    * update bottle
    
    Co-authored-by: Jeroen Ooms <jeroenooms@gmail.com>

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,15 +1,18 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://downloads.apache.org/arrow/arrow-0.17.0/apache-arrow-0.17.0.tar.gz"
-  sha256 "2c46b4c3e1f88aad510214e633a6f4ce459708f3db78cd0daf549a135cbe8e6d"
+  url "https://downloads.apache.org/arrow/arrow-0.17.1/apache-arrow-0.17.1.tar.gz"
+  # Uncomment and update to test on a release candidate
+  # url "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-0.17.1-rc1/apache-arrow-0.17.1.tar.gz"
+  sha256 "cbc51c343bca08b10f7f1b2ef15cb15057c30e5e9017cfcee18337b7e2da9ea2"
 
   bottle do
     cellar :any
-    sha256 "cca082504d26c682d50db193a340c907f2100e48bf5cf71fe478c94c38e9fcdf" => :el_capitan
+    sha256 "e31e367003c68535202852069626a9868de5338820e8b48fda3ad42058185add" => :el_capitan
     root_url "https://autobrew.github.io/bottles"
   end
 
+  depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "lz4"
   depends_on "thrift"
