04aa015191e4ba05373d85192b18a27723c86fc4
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-28 15:11:20 +0200

    ffmpeg 4.2.4 (#26)
    * ffmpeg 4.2.4
    
    * update bottles

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,14 +1,15 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.xz"
-  sha256 "cb754255ab0ee2ea5f66f8850e1bd6ad5cac1cd855d0a2f4990fb8c668b0d29c"
+  url "https://ffmpeg.org/releases/ffmpeg-4.2.4.tar.xz"
+  sha256 "0d5da81feba073ee78e0f18e0966bcaf91464ae75e18e9a0135186249e3d2a0b"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
     cellar :any_skip_relocation
     root_url "https://autobrew.github.io/bottles"
-    sha256 "ccf70655943ec26ba729f271df501f28f46b5c24738fb94e2405b825ad6fadd1" => :el_capitan
+    sha256 "996f37c818b88711a2e403b0be64422303c2373ce9ab5a4d1b028e60a8d0bb96" => :high_sierra
+    sha256 "076b58f0a2f5831bcba02aab7902b435ec1e4f261b6dbc07e9f39bf96877759f" => :el_capitan
   end
 
   depends_on "nasm" => :build
@@ -47,6 +48,9 @@ class Ffmpeg < Formula
     # Build and install additional FFmpeg tools
     system "make", "alltools"
     bin.install Dir["tools/*"].select { |f| File.executable? f }
+
+    # Fix for Non-executables that were installed to bin/
+    mv bin/"python", pkgshare/"python", force: true
   end
 
   test do
