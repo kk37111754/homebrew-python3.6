fb53bff1221c878bd47ff4fb0a64546b517f1007
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-04 13:25:00 +0200

    Update ImageMagick 6

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,9 +4,9 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.9-39.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.9-39.tar.xz"
-  sha256 "56130c16790d8d38c3a1888cc07074d5b50ffb718243e0c56e4c88839edbf931"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-32.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-32.tar.xz"
+  sha256 "151733e004cb1b49c77e04e1257b9883b6cce2221fc44b0176845e14f75eca52"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
@@ -17,47 +17,19 @@ class ImagemagickAT6 < Formula
 
   keg_only :versioned_formula
 
-  option "with-fftw", "Compile with FFTW support"
-  option "with-hdri", "Compile with HDRI support"
-  option "with-opencl", "Compile with OpenCL support"
-  option "with-openmp", "Compile with OpenMP support"
-  option "with-perl", "Compile with PerlMagick"
-  option "without-magick-plus-plus", "disable build/install of Magick++"
-  option "without-threads", "Disable threads support"
-
-  deprecated_option "enable-hdri" => "with-hdri"
-  deprecated_option "with-gcc" => "with-openmp"
-  deprecated_option "with-jp2" => "with-openjpeg"
-
   depends_on "pkg-config" => :build
+  depends_on "gettext" => :test # via cairo
   depends_on "libtool"
   depends_on "xz"
-
-  depends_on "jpeg" => :recommended
-  depends_on "libpng" => :recommended
-  depends_on "libtiff" => :recommended
-  depends_on "freetype" => :recommended
-  depends_on "fontconfig" => :recommended
-  depends_on "librsvg" => :recommended
-  depends_on "openjpeg" => :recommended
-  depends_on "pango" => :recommended
-  depends_on "webp" => :recommended
-
-  depends_on "little-cms" => :optional
-  depends_on "little-cms2" => :optional
-  depends_on "libwmf" => :optional
-  depends_on "liblqr" => :optional
-  depends_on "openexr" => :optional
-  depends_on "ghostscript" => :optional
-  depends_on "fftw" => :optional
-  depends_on "perl" => :optional
-
-  if build.with? "openmp"
-    depends_on "gcc"
-    fails_with :clang
-  end
-
-  skip_clean :la
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "freetype"
+  depends_on "fontconfig"
+  depends_on "librsvg"
+  depends_on "openjpeg"
+  depends_on "pango"
+  depends_on "webp"
 
   def install
     args = %W[
@@ -67,48 +39,22 @@ class ImagemagickAT6 < Formula
       --disable-silent-rules
       --enable-shared
       --enable-static
+      --with-freetype
+      --with-fontconfig
+      --with-openjp2
+      --with-webp
+      --with-pango
+      --with-rsvg
+      --without-x
+      --without-wmf
       --without-modules
+      --without-gslib
+      --without-openmp
+      --without-fftw
+      --without-perl
       --enable-zero-configuration
+      --with-gs-font-dir=/usr/local/share/ghostscript/fonts
     ]
-
-    if build.with? "opencl"
-      args << "--enable-opencl"
-    else
-      args << "--disable-opencl"
-    end
-
-    if build.with? "openmp"
-      args << "--enable-openmp"
-    else
-      args << "--disable-openmp"
-    end
-
-    if build.with? "webp"
-      args << "--with-webp=yes"
-    else
-      args << "--without-webp"
-    end
-
-    if build.with? "openjpeg"
-      args << "--with-openjp2"
-    else
-      args << "--without-openjp2"
-    end
-
-    args << "--without-gslib" if build.without? "ghostscript"
-    args << "--with-perl" << "--with-perl-options='PREFIX=#{prefix}'" if build.with? "perl"
-    args << "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts" if build.without? "ghostscript"
-    args << "--without-magick-plus-plus" if build.without? "magick-plus-plus"
-    args << "--enable-hdri=yes" if build.with? "hdri"
-    args << "--without-fftw" if build.without? "fftw"
-    args << "--without-pango" if build.without? "pango"
-    args << "--without-threads" if build.without? "threads"
-    args << "--with-rsvg" if build.with? "librsvg"
-    args << "--without-x" if build.without? "x11"
-    args << "--with-fontconfig=yes" if build.with? "fontconfig"
-    args << "--with-freetype=yes" if build.with? "freetype"
-    args << "--enable-zero-configuration" if build.with? "zero-configuration"
-    args << "--without-wmf" if build.without? "libwmf"
 
     # versioned stuff in main tree is pointless for us
     inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
@@ -120,7 +66,7 @@ class ImagemagickAT6 < Formula
     assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
     # Check support for recommended features and delegates.
     features = shell_output("#{bin}/convert -version")
-    %w[Modules freetype jpeg png tiff].each do |feature|
+    %w[fontconfig pango cairo rsvg webp freetype jpeg jp2 png tiff].each do |feature|
       assert_match feature, features
     end
   end
